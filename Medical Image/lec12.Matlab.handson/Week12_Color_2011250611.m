dc38b59340b3ff0ee2a4eab4d91d004e00f7fbf2
Sharku2011 <pjsw007@naver.com>
2017-12-07 09:22:47 +0900

    의료영상 과제 업데이트
    업뎃

---
+++ Medical Image/lec12.Matlab.handson/Week12_Color_2011250611.m
@@ -0,0 +1,26 @@
+close all hidden; clear; clc;
+
+pet = imread('PET.jpg');
+
+figure;
+subplot(2,2,4), imshow(pet);
+for i=1:3
+    subplot(2,2,i), imshow(pet(:,:,i));
+end
+
+pet_noise = imnoise(pet,'salt & pepper');
+
+figure;
+subplot(2,2,4), imshow(pet_noise);
+for i=1:3
+    subplot(2,2,i), imshow(pet_noise(:,:,i));
+end
+
+pet_med = zeros(size(pet));
+
+figure;
+temp = [];
+for i=1:3
+    temp = cat(2, temp, medfilt2(im2double(pet_noise(:,:,i))));
+end
+imshow(temp);
